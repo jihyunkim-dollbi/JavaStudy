0cda8560160c4206e9a42d8777ecdac44f4a6188
sist <sist@DESKTOP-RVDJDNC>
2019-12-10 16:12:50 +0900

    첫째날

---
+++ FirstProject/src/연습.java
@@ -0,0 +1,40 @@
+
+public class 연습 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+
+
+		int a=5;
+		double b=30.0; 
+		char c='김';
+
+		System.out.printf("first is %d second is %0.2f third is %c\n", a, b, c);   //    x 변수명 그대로 입력		
+
+		System.out.printf("정수는 %05d 실수는  %.3f 문자는 %c이다.\n", 5, 30.5, '김'); //  o 변수값 그대로 입력
+ 
+		System.out.printf("정수와 실수를 곱하면 %f가 된다.", a*b);                   //    o 변수의 연산
+		
+		
+		 
+		// int ree = (int)a;     
+		
+		// System.out.println(rer);
+		// System.out.println(ree);
+		
+		
+	     ///	1년 1월 1일
+		 ///       ~
+		 ///  2019년 12월 9일
+	     /// 총 일수/7 나머지 0~6   376570+343
+		
+		 int aa = 376913;
+		  
+		
+		
+		
+		
+	}
+
+}
