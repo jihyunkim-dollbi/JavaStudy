d0a47e7363fc2710993eef27230adce7d7df4533
sist <sist@DESKTOP-RVDJDNC>
2019-12-27 16:27:56 +0900

    수정

---
+++ 20191227-2019년마지막수업/src/Meaningofstaticandpublic.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ *   			(1)프로젝트 파일 
+ *   				========> 안에
+ *   			(2) src 폴더/패키지 (server 폴더1, client 폴더2, common 폴더3 ...... )
+ *    				============> 안에
+ *    			(3) 파일 
+ *                  ===================> (Login.java ,GameRoom.java,WaitRoom.java....)                 
+ * 
+ *	  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class Meaningofstaticandpublic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+	}
+
+}
