6ede3342f9e664e189b7cedcb56467e49358e777
sist <sist@DESKTOP-RVDJDNC>
2020-01-13 17:58:38 +0900

    20200113

---
+++ 20200113-최종프로젝트/src/com/sist/common/Function.java
@@ -0,0 +1,28 @@
+package com.sist.common;
+
+public class Function {
+
+	public static final int LOGIN = 100;  // 로그인 처리
+	public static final int MYLOD = 110;  // 대기실로 창 바뀜
+	
+	
+	
+	
+	/*	
+	 * Server
+	 * Function.LOGIN ==> id|pwd|sex|loc ==> waitVc 에 저장 예정
+	 * 
+	 * Client
+	 * Function.LOGIN ==> id|pwd|sex|loc ==> 테이블에 출려하는 과정
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
