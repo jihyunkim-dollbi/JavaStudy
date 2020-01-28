81d41f667033b55cd05d838ac58876084c4fdd57
sist <sist@DESKTOP-RVDJDNC>
2020-01-28 17:52:00 +0900

    20200128

---
+++ 20200123/src/MainClass.java
@@ -0,0 +1,31 @@
+/*
+ * AI- 정형화되어있지 않은 파일을 제어
+ * 
+ * 제네릭 - 타입의 변환....
+ * 
+ *  E & T ==> CLASS를 다룸
+ *  
+ *  K & V ==> Map 
+ * 
+ * 자료구조 = list & map
+ * 
+ * ArrayList! ==> 데이터를 모을때 중요!!
+ * 
+ * 
+ * 
+ * <웹>
+ * 1. 데이터 => 메모리저장 -> 본인만 사용할때
+ * 		   => 웹에는 공유(동일한 내용)/ 한군데 저장되어야 ==> 오라클사용 (데이터관리자) ==> 데이터 제어필요-읽기,쓰기(자바가..)
+ * 		   => 데이터제어- 자바, 데이터관리- 오라클, 화면UI- html,css
+ * 		   => 
+ *  * 
+ * 
+ */
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
