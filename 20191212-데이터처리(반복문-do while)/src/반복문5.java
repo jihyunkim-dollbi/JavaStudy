a95a7458b57e05bd027ca9c804604b5ad6418148
Jihyun Kim <58684292+jihyunkim-dollbi@users.noreply.github.com>
2020-03-22 19:54:15 +0900

    do-while
    utf-8

--- 20191212-데이터처리(반복문-do while)/src/반복문5.java
+++ 20191212-데이터처리(반복문-do while)/src/반복문5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class �ݺ���5 {
+public class 반복문5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,44 +15,42 @@ public class �ݺ���5 {
 		
 		do {
 			
-			if(i == 1)    //do while �ȿ� if �� else�� �� ������  ù��° ������ ù���� �����Է¹ް�, �ι�° ������ �ι�° ������ �Է¹޴´�..
-				         // �ݺ���4 ó�� do �ȿ� ���๮�� �ΰ� ������ �Ѳ����� ������ �ΰ��� ���ÿ� ���´�.
+			if(i == 1)    //do while 안에 if 와 else를 준 이유는  첫번째 바퀴에 첫번 정수입력받고, 두번째 바퀴에 두번째 정수를 입력받는다..
+				         // 반복문4 처럼 do 안에 실행문에 두개 정수를 한꺼번에 적으면 두개가 동시에 나온다.
 			{
 				
-				System.out.println("ù��° ���� �Է�: ");
+				System.out.println("첫번째 정수 입력: ");
 				num1 = scan.nextInt();
 			}
 			else {
 				
 				
-				System.out.println("�ι�° ���� �Է�: ");
+				System.out.println("두번째 정수 입력: ");
 				num2 = scan.nextInt();
 			}
-			
-			
-			
+
 			i++;
 			
 		}while(i<=2);
 		
 		if(num1 > num2)
 			
-			System.out.println("�ִ밪: " + num1+ ", �ּҰ�: " + num2);
+			System.out.println("최대값: " + num1+ ", 최소값: " + num2);
 			
 		
 		else 
 			
-			System.out.println("�ִ밪: " + num2 + ", �ּҰ�: " + num1);
+			System.out.println("최대값: " + num2 + ", 최소값: " + num1);
 		
-		System.out.println("�ִ밪: " + (num1>num2?num1:num2) + ", �ּҰ�: " + (num1>num2?num2:num1));
+		System.out.println("최대값: " + (num1>num2?num1:num2) + ", 최소값: " + (num1>num2?num2:num1));
 
 		
-		System.out.println("�ִ밪: " + (Math.max(num1, num2)) + ", �ּҰ�: " + (Math.min(num1, num2)));
+		System.out.println("최대값: " + (Math.max(num1, num2)) + ", 최소값: " + (Math.min(num1, num2)));
 		
 		
-		/*    "�ݺ���4"���� ó���� �Ǽ��� do �ȿ� �ٷ� ���๮�� ���� ù��° , �ι�° ������ ���ÿ� ������,
-        "�ݺ���5"������ if else�� ���� ���๮�� �����ߴ�.-- but �׷��ʿ����
-        "�ݺ��� 6"���� �����Է� ���๮�� do �ۿ� ó���� ��� �ξ� �����ϰ� ó���Ͽ���.
+		/*    "반복문4"에서 처음의 실수는 do 안에 바로 실행문을 쓰니 첫번째 , 두번째 정수가 동시에 나오고,
+        "반복문5"에서는 if else를 만들어서 실행문을 적용했다.-- but 그럴필요없이
+        "반복문 6"에서 정수입력 실행문을 do 밖에 처음에 적어서 훨씩 간결하게 처리하였다.
         */
 		
 		
